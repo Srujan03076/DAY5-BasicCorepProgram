38b70b4a9476fd7beeae88f8f531246ec61f9d25
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-11 11:48:52 +0530

    Adding Harmonic number program

--- Harmonic number.cs
+++ Harmonic number.cs
@@ -4,25 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-    namespace Day_5_PowerofTwo
+namespace Day_5_HarmonicNumberSeries
 {
-    class UC_3_PowerOfTwo
+    class UC_4_HarmonicSeries
     {
         static void Main(string[] args)
         {
 
+
+
             int Num = 0;
             int i = 1;
-            Console.WriteLine("Enter the Number ");
+            double sumofHarmonicSeries = 0.0;
+            Console.WriteLine("Enter the number");
             Num = int.Parse(Console.ReadLine());
+
             for (i = 1; i <= Num; i++)
             {
 
+                Console.Write("1/{0} +", i);
+                sumofHarmonicSeries = 1 / (float)i;
 
-                double power = Math.Pow(2, i);
-                Console.WriteLine("Power of Two = " + power);
+
+
 
             }
+            Console.WriteLine("\nSum of series upto {0} terms : {1}\n", +Num, +sumofHarmonicSeries);
             Console.ReadLine();
         }
     }
