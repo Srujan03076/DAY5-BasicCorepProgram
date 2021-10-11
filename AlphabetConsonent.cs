fd0a33e31457b6e6253b7df337f59d87fbe961b1
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-11 12:38:17 +0530

    Adding Vowel Consonent \program

---
+++ AlphabetConsonent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_AlphabetVowelorConsonent
+{
+    class UC_9_CheckTheVowelOrConsonant
+    {
+        static void Main(string[] args)
+        {
+
+            char Alphabet;
+            
+
+            Console.WriteLine("Enter the Alphabet");
+            Alphabet = char.Parse(Console.ReadLine());
+
+            if ( Alphabet == 'a' || Alphabet == 'e'|| Alphabet == 'i' || Alphabet == 'o' || Alphabet == 'u' )  
+            { 
+                Console.WriteLine("Alphabet is a vowel");
+
+            }
+            else
+            {
+                Console.WriteLine("Alphabet is a consonant");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
