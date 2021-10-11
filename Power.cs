8d78a4bd714b5ce654bf26d7ca57af6d076e6521
unknown <srujanmahaja5@gmail.com>
2021-10-11 11:30:26 +0530

    Adding Power of 2 program

---
+++ Power.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Day_5_PowerofTwo
+{
+    class UC_3_PowerOfTwo
+    {
+        static void Main(string[] args)
+        {
+
+            int Num = 0;
+            int i = 1;
+            Console.WriteLine("Enter the Number ");
+            Num = int.Parse(Console.ReadLine());
+            for (i = 1; i <= Num; i++)
+            {
+
+
+                double power = Math.Pow(2, i);
+                Console.WriteLine("Power of Two = " + power);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
