bacc09cd82473ae412dec1e4f9b0d150f914f110
unknown <srujanmahaja5@gmail.com>
2021-10-11 10:57:57 +0530

    Adding Leap Year program

---
+++ Leapyear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Day5___Flipcoin
+{
+
+    class LeapYear
+    {
+
+        public static void leap()
+        {
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 400) == 0) || ((year % 4) == 0) && ((year % 100) != 0))
+            {
+                Console.WriteLine("{0} is a leap year", +year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year", +year);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LeapYear.leap();
+            Console.ReadLine();
+        }
+    }
+}
+    
+    
